b865eb8b4910f4d682ea4cb497fb1526b0759e4d
Allwka <bryanjaramillobaldeon@gmail.com>
2019-10-15 19:49:43 +0100

    working on pc

---
+++ YouSayWhatServer/src/controllers/roomsController.js
@@ -0,0 +1,10 @@
+module.exports = { getRooms }
+function getRooms(req, res) {
+    res.send("asd")
+}
+function delRoom(req, res) {
+
+}
+function joinRoom(req, res) {
+
+}
--- YouSayWhatServer/src/routes/routes.js
+++ YouSayWhatServer/src/routes/routes.js
@@ -1,4 +1,10 @@
 const router = require("express").Router();
 
+const roomController = require("../controllers/roomsController");
+const userController = require("../controllers/usersController");
+
+const roomUri = '/rooms'
+//ROOMS ROUTES
+router.get(roomUri, roomController.getRooms)
 
 module.exports = router;
