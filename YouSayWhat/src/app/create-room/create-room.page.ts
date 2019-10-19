62e5e1b0151e517bdef37acd4d6dbd3c383804a6
Allwka <bryanjaramillobaldeon@gmail.com>
2019-10-19 18:13:59 +0100

    room created

--- YouSayWhat/src/app/create-room/create-room.page.ts
+++ YouSayWhat/src/app/create-room/create-room.page.ts
@@ -7,32 +7,72 @@ import axios from "axios";
 })
 export class CreateRoomPage implements OnInit {
 
-  URI: string = "http://localhos:3000/api/v1/rooms";
+  roomURI: string = "http://localhost:3000/api/v1/rooms";
+  userURI: string = "http://localhost:3000/api/v1/users"
   roomParameters: Object = {
     name: "",
     maxPlayers: 2,
     pwd: "",
-  }
+  };
   userParameters: Object = {
     nick: ""
-  }
+  };
 
   constructor() { }
 
   ngOnInit() {
   }
-  createRoom() {
-    axios.post(this.URI, {})
+  deleteRoom(id: string) {
+    axios.delete(this.roomURI + "/" + id)
       .then(room => {
-
+        console.log(room)
+        this.joinRoom();
       })
       .catch(err => alert(err))
   }
-  joinRoom() {
-
+  createRoom(): void {
+    axios.post(this.roomURI, this.roomParameters)
+      .then((room) => {
+        console.log(room)
+        if (room) {
+          if (this.joinRoom()) {
+            console.log("joined")
+          } else {
+            this.deleteRoom(room["data"]["_id"]);
+          }
+        } else {
+          alert("room not created")
+        }
+      })
+      .catch(err => alert(err))
   }
-  validations() {
-
+  createUser() {
+    return axios.post(this.userURI, this.userParameters)
+      .then(user => {
+        if (user) {
+          console.log(user)
+          return true
+        } else {
+          return false
+        }
+      })
+      .catch(err => {
+        alert(err);
+        return false;
+      })
   }
+  joinRoom(): Boolean {
+    console.log("joing")
+    return false;
+  }
+  validations(): Boolean {
+    if (this.roomParameters["name"] != "" && this.roomParameters["pwd"] != "" && this.userParameters["nick"] != "") {
+      return true;
+    } else {
+      alert("some field is null")
+      return false;
+    }
+  }
+
 
 }
--- YouSayWhatServer/src/controllers/roomsController.js
+++ YouSayWhatServer/src/controllers/roomsController.js
@@ -5,15 +5,16 @@ module.exports = { getRooms, delRoom, createRoom }
 function getRooms(req, res) {
     return roomModel.find()
         .then(rooms => res.json(rooms))
-        .catch(err => res.json(err))
+        .catch(err => res.status(400).json(err))
 }
 function delRoom(req, res) {
+    console.log(req.params)
     return roomModel.findByIdAndDelete(req.params.id)
         .then(room => res.json(room))
-        .catch(err => res.json(err));
+        .catch(err => res.status(400).json(err));
 }
 function createRoom(req, res) {
     return roomModel.create(req.body)
         .then(room => res.json(room))
-        .catch(err => res.json(err));
+        .catch(err => res.status(400).json(err));
 }
